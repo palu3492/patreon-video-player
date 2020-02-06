03865e06cd8c4803d73f80d33cb536102ca736da
Alex Palumbo <palu3492@stthomas.edu>
2020-02-05 20:08:26 -0600

    Update

--- js/countImpressions.js
+++ js/countImpressions.js
@@ -1,6 +1,6 @@
 
 let xhr = new XMLHttpRequest();
-let counterApiUrl = 'https://cors-anywhere.herokuapp.com/https://bit.ly/2NRJxgf';
+let counterApiUrl = 'https://cors-anywhere.herokuapp.com/https://bit.ly/2CnMtft'; // https://www.simple-counter.com/hit.php?id=zvmnffc // 11-8-19 it was 7
 function countImpression() {
     xhr.open("GET", counterApiUrl, true);
     xhr.setRequestHeader('Content-Type', 'text/html');
